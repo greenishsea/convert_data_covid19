4457326fbb513fea00c47c17f24d760d402d3091
greenishsea <greenishseajp@gmail.com>
2021-08-10 00:27:00 +0900

    Fix filtering by date scope

--- logics/logic.py
+++ logics/logic.py
@@ -1,20 +1,25 @@
 import datetime
 import json
+import pandas as pd
 from pandas.io.json import json_normalize
 
 
 def convert_data(infile_path, outfile_path):
     today = datetime.date.today()
-    today_string = today.strftime('%Y-%-m-%-d')
-    start_date_string = (today + datetime.timedelta(days=-180)).strftime('%Y-%-m-%-d')
+    today_string = today.strftime('%Y-%m-%d')
+    start_date_string = (today + datetime.timedelta(days=-180)).strftime('%Y-%m-%d')
 
     data = None
     with open(infile_path) as inf:
         data = json.load(inf)
         for k, v in data.items():
             df = json_normalize(v)
-            mask = (df['date'] > start_date_string) & (df['date'] <= today_string)
-            filtered_df = df.drop(columns='recovered').loc[mask]
+            # Remove recovered data
+            df = df.drop(columns='recovered')
+            # Regularize date string to zero padding
+            df['date'] = pd.to_datetime(df['date']).dt.strftime('%Y-%m-%d')
+            mask = (df['date'] >= start_date_string) & (df['date'] <= today_string)
+            filtered_df = df.loc[mask]
             data[k] = filtered_df.to_dict('r')
 
     with open(outfile_path, 'w') as outf:
